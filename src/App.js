c4f5b9d08746f3ba083374919050e7016fc8a7b8
romkravets <romann.kravets@gmail.com>
2020-05-22 18:10:05 +0300

    add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
+
+callAPI() {
+  fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+  this.callAPI();
+}
   render() {
     return (
       <div className="App">
@@ -13,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">;{this.state.apiResponse}</p>
       </div>
     );
   }
