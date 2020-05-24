fcccce30dba65957cd4fb87942683575bb00d1da
romkravets <romann.kravets@gmail.com>
2020-05-24 15:44:01 +0300

    Update Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = (props) => {
       <nav className="nav-wrapper grey darken-3">
          <div className="container">
             <Link to="/" className="brand-logo">Market</Link>
-            {links}
+            {auth.isLoaded && links}
          </div>
       </nav>
    )
