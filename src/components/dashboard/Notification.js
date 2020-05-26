f4a3f29f3b2d309a237fc3768f254a9afac23f82
romkravets <romann.kravets@gmail.com>
2020-05-26 17:10:36 +0300

    Cloud Function Firestore Triggers

--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Notifications = () => {
+const Notifications = (props) => {
    return (
-      <div>
-         <p>Notifications</p>
+      <div class="section">
+         <div className="card z-depth-0">
+            <div className="card-content">
+               <span className="card-title">Notification</span>
+               <ul className="notifications">
+                  <li>Notification</li>
+                  <li>Notification</li>
+                  <li>Notification</li>
+                  <li>Notification</li>
+               </ul>
+            </div>
+         </div>
       </div>
    )
 }
