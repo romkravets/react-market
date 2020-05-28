34e1a80f2ff8cb6a7595108c405899e65910d20e
romkravets <romann.kravets@gmail.com>
2020-05-28 13:48:02 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,88 @@
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// admin.initializeApp(functions.config().firebase);
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello, users!");
+// });
+
+// const createNotifocation = (notification => {
+//    return admin.firestore().collection(`notifications`)
+//    .add(notification)
+//    .then(doc => console.log('notification added', doc));
+// });
+
+// exports.projectCreated = functions.firestore
+//    .document('projects/{projectId}')
+//    .onCreate(doc => {
+
+//       const project = doc.data();
+//       const notification = {
+//          content: 'Added a new product',
+//          user: `${project.authorFirstName} ${project.authorLastName}`,
+//          time: admin.firestore.FieldValue.serverTimestamp()
+//       }
+//       return createNotifocation(notification);
+// });
+
+// exports.userJoined = functions.auth.user()
+//   .onCreate(user => {
+    
+//     return admin.firestore().collection('users')
+//       .doc(user.uid).get().then(doc => {
+
+//         const newUser = doc.data();
+//         const notification = {
+//           content: 'Joined the party',
+//           user: `${newUser.firstName} ${newUser.lastName}`,
+//           time: admin.firestore.FieldValue.serverTimestamp()
+//         };
+
+//         return createNotification(notification);
+
+//       });
+// });
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello, users!");
+const createNotification = ((notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
 });
 
-const createNotifocation = (notification => {
-   return admin.firestore().collection(`notifications`)
-   .add(notification)
-   .then(doc => console.log('notification added', doc));
-})
 
 exports.projectCreated = functions.firestore
-   .document('projects/{projectId}')
-   .onCreate(doc => {
+  .document('projects/{projectId}')
+  .onCreate(doc => {
 
-      const project = doc.data();
-      const notification = {
-         content: 'Added a new product',
-         user: `${project.authorFirstName} ${project.authorLastName}`,
-         time: admin.firestore.FieldValue.serverTimestamp()
-      }
-      return createNotifocation(notification);
-})
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification);
+
+});
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
+});
